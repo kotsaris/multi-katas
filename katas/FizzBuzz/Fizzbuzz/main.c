76722790bc14a10753df50f2cf387561224d8069
Anapiroz <nikos240192@hotmail.com>
2014-06-06 00:01:29 +0300

    Ta ekana update sorry pou argisa slackara ligo

--- katas/FizzBuzz/Fizzbuzz/main.c
+++ katas/FizzBuzz/Fizzbuzz/main.c
@@ -8,12 +8,15 @@ int main(int argc, char *argv[]) {
 	for(i=1; i<=100; i++)
 
 		
-		if (i % 5==0){
+		 if ((i % 3==0) && (i % 5==0)){
+			printf("fizzbuzz\n");
+		}
+	else if (i % 3==0){
 			printf("fizz\n");
 		}
-	else	if (i % 3==0){
-			printf("buzz\n");
-		}
+	else if (i % 5==0){
+		printf("buzz\n");
+	}
 	else printf("%d \n",i);
 	getch();
 	return 0;
